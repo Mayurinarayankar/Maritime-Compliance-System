44c3b4ca58ce06cb6789899014825c2645dd3fe7
Mayuri Narayankar <mayuri.narayankar@webitoinfotech.com>
2026-05-06 18:34:46 +0530

    Completed DB setup and initial migration

---
+++ MaritimeApp.Infrastructure/Data/AppDbContextFactory.cs
@@ -0,0 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace MaritimeApp.Infrastructure.Data
+{
+    public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+
+            optionsBuilder.UseNpgsql(
+                "Host=localhost;Port=5432;Database=maritime_db;Username=postgres;Password=Mayu@9049"
+            );
+
+            return new AppDbContext(optionsBuilder.Options);
+        }
+    }
+}
